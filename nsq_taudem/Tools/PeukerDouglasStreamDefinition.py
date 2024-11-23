80aac9af9895a338aee69dd579bbed510d2c72e5
Shihab E. <43100312+Shihabyooo@users.noreply.github.com>
2024-11-23 14:16:43 +0400

    Switched to dynamic loading of new staged tool desc, some minor work on PeukerDouglas stream def.

--- nsq_taudem/Tools/PeukerDouglasStreamDefinition.py
+++ nsq_taudem/Tools/PeukerDouglasStreamDefinition.py
@@ -1,5 +1,4 @@
-#Probably a waste to have this tool, as it simply just calls QGIS/GDAL's Polygonize.
-#Implies 8 connectedness and name of field = ("value")
+#This tool stages the following tools: PeukerDouglas -> AreaD8 -> (Optional)DropAnalysis -> Threshold
 
 from .AlgorithmGenerator import Algorithm
 from ..helpers import Utilities
@@ -16,6 +15,7 @@ class StagedAlgorithm(Algorithm):
     #TODO use the name that is used for each called TauDEM tool.
     DEM = "dem"
     FDR = "fdr"
+    D8_CONTRIB = "d8contrib"
     WCENTER = "wcenter"
     WSIDE = "wside" 
     WDIAG = "wdiag"
@@ -24,6 +24,10 @@ class StagedAlgorithm(Algorithm):
     OUTLETS = "outlets"
     MASK = "mask"
     USE_THRESH = "usethresh"
+    MIN_THRESH = "minthres"
+    MAX_THRESH = "maxthresh"
+    NUM_THRESH = "numthresh"
+    USE_LOG = "uselog"
     STR_SRC = "strsrc"
     FAC = "fac"
     STR_GRD = "strgrd"
@@ -79,11 +83,39 @@ class StagedAlgorithm(Algorithm):
                                                             description = "Mask grid",
                                                             optional = True,
                                                             defaultValue = [QgsProcessing.TypeRaster]))
+        # D8 conributing area (optional) = r
+        self.addParameter(QgsProcessingParameterRasterLayer(name = self.D8_CONTRIB,
+                                                            description = "D8 contributing area for drop analysis",
+                                                            optional = True,
+                                                            defaultValue = [QgsProcessing.TypeRaster]))
         # Use the range below to automatically select threshold by drop analysis = False
         self.addParameter(QgsProcessingParameterBoolean(name = self.USE_THRESH,
                                                         description = "Use the range below to automatically select threshold by drop analysis",
                                                         optional = False,
                                                         defaultValue = False))
+        # Minimum threshold = 5
+        self.addParameter(QgsProcessingParameterNumber(name = self.MIN_THRESH,
+                                                            description = "Minimum threshold value (Drop analysis)",
+                                                            optional = False,
+                                                            defaultValue = 5,
+                                                            type = QgsProcessingParameterNumber.Integer))
+        # Maximum threshold = 500
+        self.addParameter(QgsProcessingParameterNumber(name = self.MAX_THRESH,
+                                                            description = "Maximum threshold value (Drop analysis)",
+                                                            optional = False,
+                                                            defaultValue = 500,
+                                                            type = QgsProcessingParameterNumber.Integer))
+        # Number of threshold values = 10
+        self.addParameter(QgsProcessingParameterNumber(name = self.NUM_THRESH,
+                                                            description = "Number of threshold values (Drop analysis)",
+                                                            optional = False,
+                                                            defaultValue = 10,
+                                                            type = QgsProcessingParameterNumber.Integer))
+        # Logarithmic spacing = True
+        self.addParameter(QgsProcessingParameterBoolean(name = self.USE_LOG,
+                                                        description = "Logarithmic spacing (Drop analysis)",
+                                                        optional = False,
+                                                        defaultValue = True))
         #This tool does call other TauDEM tools, so add processes count option
         self.AddProcessCountInputParam()
 
@@ -97,16 +129,6 @@ class StagedAlgorithm(Algorithm):
 
 
     def processAlgorithm(self, parameters, context, feedback):
-         #{"desc" : str = description of the input
-            # "option" : str  = cli option
-            # "type" : str =
-            # "isOptional" : bool =
-            # "type" : str =
-            # "default" : int/float/bool =
-            #}
-        #def EvaluateQGISInputParameter(self, param, paramList, context) -> list:
-        #pType = param["type"].lower()
-        #args = {"parameters" : paramList, "name" : self.ParameterName(param) , "context" : context}
         inputs = {}
         outputs = {}
         
@@ -119,7 +141,12 @@ class StagedAlgorithm(Algorithm):
         inputs[self.CHECK_EDGE] = len(self.EvaluateQGISInputParameter({"type" : "b", "option" : "", "desc" : self.CHECK_EDGE}, parameters, context)) > 0
         inputs[self.OUTLETS] = self.EvaluateQGISInputParameter({"type" : "v0", "option" : "", "desc" : self.OUTLETS}, parameters, context)
         inputs[self.MASK] = self.EvaluateQGISInputParameter({"type" : "r", "option" : "", "desc" : self.MASK}, parameters, context)
+        inputs[self.D8_CONTRIB] = self.EvaluateQGISInputParameter({"type" : "r", "option" : "", "desc" : self.D8_CONTRIB}, parameters, context)
         inputs[self.USE_THRESH] = len(self.EvaluateQGISInputParameter({"type" : "b", "option" : "", "desc" : self.USE_THRESH}, parameters, context)) > 0
+        inputs[self.MIN_THRESH] = self.EvaluateQGISInputParameter({"type" : "i", "option" : "", "desc" : self.MIN_THRESH}, parameters, context)
+        inputs[self.MAX_THRESH] = self.EvaluateQGISInputParameter({"type" : "i", "option" : "", "desc" : self.MAX_THRESH}, parameters, context)
+        inputs[self.NUM_THRESH] = self.EvaluateQGISInputParameter({"type" : "i", "option" : "", "desc" : self.NUM_THRESH}, parameters, context)
+        inputs[self.USE_LOG] = len(self.EvaluateQGISInputParameter({"type" : "b", "option" : "", "desc" : self.USE_LOG}, parameters, context)) > 0
 
         outputs[self.STR_SRC] = self.parameterAsOutputLayer(parameters, self.STR_SRC, context)
         outputs[self.FAC] = self.parameterAsOutputLayer(parameters, self.FAC, context)
@@ -129,11 +156,34 @@ class StagedAlgorithm(Algorithm):
         
         #validate inputs
         for key in inputs.keys():
-            if key not in [self.OUTLETS, self.MASK] and inputs[key] is None:
+            if key not in [self.OUTLETS, self.MASK, self.D8_CONTRIB] and inputs[key] is None:
                 feedback.pushInfo(f"Error! Could not evaulate inputs {key}")
                 raise QgsProcessingException(self.invalidSourceError(parameters, key))
         
         #TODO validate output path generation 
-         
-                
+
+        #note: ommit optional input from commands bellow if not entered by user
+        #first run PeukerDouglas (in DEM, in WCenterm in Wside, in Wdiag,  out StrSrc)
+            #strsrc is now an input to other tools
+        #run AreaD8 (in FDR, in Oulets, in CheckEdge, out FAC) 
+            #Note: This FAC is an AD8 FAC.
+        #if USETHRESH is set AND Outlets are provided:
+            #run DropAnalaysis (in DEM, in FDR, in D8Contrib in FAC, in outlets, in minthresh, in maxthresh, in numthres, in UseLog, out dropfile)
+
+        #process drop file
+            #Drop file has many entires, what we need is the value at the last line, which reads:
+            #Optimium Threshold Value: (Threshold as floating number)
+            #The arcpy implementation simply opens the file, grabs all content, then splits (using
+            #rsplit) once with space as delimited (i.e. dropFileContent.rsplot(" ", 1)) and takes the
+            #second entry from the resulting list, which the floating number above.
+        
+        #if USETHRESH and Oulets is given:
+            #threshold = threshold from drop analysis
+        #else
+            #threshold = ACC_THRESH input by user
+
+        #run Threshold (in FAC, in threshold, in mask, out strsrc)
+        
+        #and we're done!
+        
         return {self.STR_SRC : outputs[self.STR_SRC], self.FAC : outputs[self.FAC], self.STR_GRD : outputs[self.STR_GRD]}
--- nsq_taudem/Tools/__init__.py
+++ nsq_taudem/Tools/__init__.py
@@ -1,1 +1,7 @@
-__all__ = ["AlgorithmGenerator", "WatershedGridToSHP", "PeukerDouglasStreamDefinition"] #TODO dynamically generate this list from directory content
+from os import listdir, path
+
+__all__ = []
+
+for file in listdir(path.dirname(__file__)):
+    if file.endswith(".py"):
+        __all__.append(file[0:-3])
